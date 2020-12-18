2e1b4b888f9ef2b2cd6bbb1198279c5f613e4e81
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-12-18 11:07:32 +0530

    added register endpoint

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ const app = express()
 //gettig json db instance
 const db = new JsonDB(new Config('JsonDB', true, false, '/'));
 
+//adding middleware
+app.use(express.json())
+
+app.get('/register', (req, res) => {
+  try {
+    const { username,password } = req.body
+    const id = uuid.v4()
+    const temp_secret = speakeasy.generateSecret()
+    db.push(`/users/${id}`, { id, username, password, temp_secret, is_verified: false }, true)
+    res.status(200).json({ message: 'User registered successfully!', id })
+  } catch (e) {
+    res.status(500).json({ message: 'Error Occured while registering!' })
+  }
+})
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
